0d2f29e02ec4aef0d44d76c7f98908ffcd5bdb0f
BigBoy <moshazly416@gmail.com>
2025-03-03 22:14:54 +0200

    setup desgin

---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Traits;
+trait ApiResponseTrait
+{
+    public function successResponse($data, $code = 200)
+    {
+
+    }
+
+    public function errorResponse($data, $code = 400)
+    {
+
+    }
+}
