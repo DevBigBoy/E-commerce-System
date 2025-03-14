0fa2648c75910f05c931ffadc230d2a324094146
BigBoy <moshazly416@gmail.com>
2025-03-14 23:51:04 +0200

    implement Login Via Socialite - Github

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Socialite\GithubAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::prefix('github')->name('github.')->controller(GithubAuthController::class)->group(function () {
+    Route::get('/auth/github', 'redirectToGithub')->name('auth.github');
+    Route::get('/auth/github/callback', 'handleGithubCallback')->name('auth.github.callback');
+});
